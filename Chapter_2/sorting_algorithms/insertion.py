bf921a10224beb6958490944ed2684b8663fa5f5
Suriya Ganesh <suriyaganesh097@gmail.com>
2021-05-05 20:16:57 +0530

    added chapter 1 and 2

--- Chapter_2/sorting_algorithms/insertion.py
+++ Chapter_2/sorting_algorithms/insertion.py
@@ -1,21 +1,14 @@
+arr = [1, 23, 1, 42, 4, 2, 132, 9]
 
-arr = [1,23,1,42,4,2,132,9]
-
-
-for i in range(1,len(arr)):
-    j = i-1
+for i in range(1, len(arr)):
+    j = i - 1
     value = arr[i]
-    # print(value)
 
-
-    while j>=0 and arr[j]>=value :
-
-        print("while")
-        #shifting right by one
-        arr[j+1] = arr[j]
-        j-=1
+    while j >= 0 and arr[j] >= value:
+        # shifting right by one
+        arr[j + 1] = arr[j]
+        j -= 1
         print(arr)
 
-    #assigning in the found spot
-    arr[j+1] = value
-
+    # assigning in the found spot
+    arr[j + 1] = value
